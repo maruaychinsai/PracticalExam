89c10123507bc3ddf3b4b2480bbb23f023aa2d02
maruaychinsai <87422618+maruaychinsai@users.noreply.github.com>
2021-07-15 22:41:23 +0800

    Delete alphabeticalorder.php

--- alphabeticalorder.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $names = array('Marvin', 'Marco', 'Marvin','Marco', 'Marco','Marvin', 'Christian');
-    sort($names);
-
-    foreach($names as $y => $x)
-    {
-        echo "Name [".$y."] =".$x."<br>";
-    }
-?>
